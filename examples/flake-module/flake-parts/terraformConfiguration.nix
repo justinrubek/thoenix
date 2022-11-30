95608a4eeec6826cf49ecbd30cda45a601c875fe
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2022-11-30 06:02:40 -0600

    chore: remove old variables from flake-module example

--- examples/flake-module/flake-parts/terraformConfiguration.nix
+++ examples/flake-module/flake-parts/terraformConfiguration.nix
@@ -14,62 +14,11 @@
     inputs',
     ...
   }: let
-    # individual terraform configurations are stored in the `terraform/configuration` directory
-    # and are referenced by their name in the configuration directory
-    terraformConfigurationDirectory = ../terraform/configurations;
-
-    # builds the terranix configuration for each terraform configuration
-    # and merges them into a single configuration derivation
-    finalConfigurations = inputs.thoenix.lib.buildTerraformConfigurations {
-      configDir = terraformConfigurationDirectory;
-      configNames = inputs.thoenix.lib.determineSubdirNames {
-        path = terraformConfigurationDirectory;
-      };
-
-      inherit pkgs system;
-    };
-
-    # we could get the configure names directly from `determineSubdirNames`, but
-    # the output of `buildTerraformConfigurations` is a set, so we can just use attrNames
-    terraformConfigurationNames = builtins.attrNames finalConfigurations;
-
-    # rename all values from finalConfigurations to be prefixed with `terraformConfiguration_`
-    # this allows them to be specified in the `packages` flake output
-    terraformConfigurationOutput = let
-      prefix = "terraformConfiguration_";
-      reducer = l: r: l // {"${prefix}${r}" = finalConfigurations.${r};};
-    in
-      builtins.foldl' reducer {} terraformConfigurationNames;
-
-    # create a JSON file containing the names of configurations as well as paths to their directories
-    # { configurations = [ { name = "core"; path = "/nix/store/..."; } ]; }
-    configurationMatrix = let
-      reducer = l: r:
-        l
-        // {
-          configurations =
-            l.configurations
-            ++ [
-              {
-                name = r;
-                path = finalConfigurations.${r};
-              }
-            ];
-        };
-      configurationJSON = builtins.toJSON (builtins.foldl' reducer {configurations = [];} terraformConfigurationNames);
-    in
-      pkgs.stdenv.mkDerivation {
-        name = "terraform-configuration-matrix";
-        buildCommand = ''
-          mkdir -p $out
-          echo '${configurationJSON}' > $out/terraform-configuration-matrix.json
-        '';
-      };
-  in rec {
+  in {
     thoenix.terraformConfigurations = {
       enable = true;
 
-      configDirectory = terraformConfigurationDirectory;
+      configDirectory = ../terraform/configurations;
     };
   };
 }
