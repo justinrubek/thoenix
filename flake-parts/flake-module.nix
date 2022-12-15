ba2962fbfbe318742bf2cbd0d3fdaeaf0b5da590
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2022-12-15 11:14:53 -0600

    refactorflakeModule packages output is namespaced using \/ instead of _

--- flake-parts/flake-module.nix
+++ flake-parts/flake-module.nix
@@ -67,10 +67,10 @@
         # the output of `buildTerraformConfigurations` is a set, so we can just use attrNames
         terraformConfigurationNames = builtins.attrNames finalConfigurations;
 
-        # rename all values from finalConfigurations to be prefixed with `terraformConfiguration_`
+        # rename all values from finalConfigurations to be prefixed with `terraformConfiguration/`
         # this allows them to be specified in the `packages` flake output
         terraformConfigurationOutput = let
-          prefix = "terraformConfiguration_";
+          prefix = "terraformConfiguration/";
           reducer = l: r: l // {"${prefix}${r}" = finalConfigurations.${r};};
         in
           builtins.foldl' reducer {} terraformConfigurationNames;
