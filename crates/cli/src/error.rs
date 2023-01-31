71f8495db97a7815423da0149fee9c09e864bb46
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2023-01-31 13:04:45 -0600

    feat: encode/decode git chunks

--- crates/cli/src/error.rs
+++ crates/cli/src/error.rs
@@ -8,6 +8,8 @@ pub enum AppError {
     Russh(#[from] russh::Error),
     #[error(transparent)]
     Io(#[from] std::io::Error),
+    #[error(transparent)]
+    FromUtf8(#[from] std::string::FromUtf8Error),
 
     // Application specific errors
     #[error("no data directory specified")]
